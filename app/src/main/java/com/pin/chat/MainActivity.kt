f9a5d6e1cf7a23f7f742e40c1bd9d902e103049d
ian herdianto <ianherdianto@outlook.com>
2019-02-18 08:48:20 +0700

    update on github

--- app/src/main/java/com/pin/chat/MainActivity.kt
+++ app/src/main/java/com/pin/chat/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.pin.chat
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -8,6 +9,7 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -40,6 +42,19 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+
+    fun loginBtnNavClicked(view: View) {
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
+    }
+
+    fun addChannelClicked(view: View) {
+
+    }
+
+    fun sendMsgBtnClicked(view: View) {
+
     }
 
     /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
